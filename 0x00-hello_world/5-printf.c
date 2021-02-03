8b8ba1352cb6bc7b8abaef28856c3751dd90842b
ForeroAlexander <foreroalex26@gmail.com>
2021-02-03 22:27:26 +0000

    C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,10 @@
  *
  * Return: Always 0 (Success)
  */
-#include <stdio.h>
+
+#include<stdio.h>
 int main(void)
 {
-	printf(""Programming is like building a multilingual puzzle");
-        return (0);
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
