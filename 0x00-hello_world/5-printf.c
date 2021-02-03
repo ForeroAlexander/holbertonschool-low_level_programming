c373d1e63f6a7d116a8d7024e8462714c1a6d5af
ForeroAlexander <foreroalex26@gmail.com>
2021-02-03 22:06:03 +0000

    C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf(""Programming is like building a multilingual puzzle");
+        return (0);
+}
