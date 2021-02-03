da2c6399e3c831609378c00d1a9c22d28d8c492d
ForeroAlexander <foreroalex26@gmail.com>
2021-02-03 22:34:31 +0000

    C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,9 @@
  *
  * Return: Always 0 (Success)
  */
-
 #include<stdio.h>
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
