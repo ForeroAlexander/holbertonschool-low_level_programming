feadbce22e883ce4ec3ef8ca995ded1d16d041d7
ForeroAlexander <foreroalex26@gmail.com>
2021-02-04 22:41:32 +0000

    C

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+		putchar(ch);
+
+	return (0);
+}
