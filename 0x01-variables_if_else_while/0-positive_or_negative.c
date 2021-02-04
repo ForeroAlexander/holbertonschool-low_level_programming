809a94952e87952bb4ad506cda84ccb1e0690dd2
ForeroAlexander <foreroalex26@gmail.com>
2021-02-04 20:27:32 +0000

    C#2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,17 +8,17 @@
  */
 int main(void)
 {
-        int n;
+	int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-        if (n == 0)
-                printf("%d is zero\n", n);
-        else if (n > 0)
-                printf("%d is positive\n", n);
-        else
-                printf("&d is negative\n", n);
+	if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n > 0)
+		printf("%d is positive\n", n);
+	else
+		printf("&d is negative\n", n);
 
-        return (0);
+	return (0);
 }
