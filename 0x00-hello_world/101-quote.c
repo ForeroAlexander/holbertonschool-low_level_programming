bd1d566d2981b91811ecb2cc681a54fdf95b9c45
ForeroAlexander <foreroalex26@gmail.com>
2021-02-04 01:12:06 +0000

    Cad

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+/**
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
+#include<stdio.h>
+
+int main(void)
+{
+	char str1[] = "and that piece of art is useful" - "Dora Korpar, 2015-10-19\n";
+
+	write(2, str1, 59);
+	return (1);
+}
