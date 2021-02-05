576acf6cc6b345462067b6440199c8bea58faf17
ForeroAlexander <foreroalex26@gmail.com>
2021-02-05 01:38:47 +0000

    C

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 0 ; n <= 9 ; n++)
+	{
+		printf("&d", n);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
