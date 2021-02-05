0caeab635aaf598d36fbcd0de835618ba183548c
ForeroAlexander <foreroalex26@gmail.com>
2021-02-05 02:12:33 +0000

    C

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+ #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z' ; ch <= 'a' ; ch++)
+	{
+                putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
