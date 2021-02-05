225e9de1e48271ff2eb2c50820d1bc3e19438613
ForeroAlexander <foreroalex26@gmail.com>
2021-02-05 02:01:59 +0000

    C

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+   #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 9 ; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+
+}
