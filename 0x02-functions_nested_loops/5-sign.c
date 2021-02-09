9c450be9e2ca562f07350aeecae4f8a2e8fe5f8f
ForeroAlexander <foreroalex26@gmail.com>
2021-02-09 23:16:53 +0000

    C

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,20 +1,26 @@
 #include "holberton.h"
 
+/**
+ * print_sign - Compare the variable N
+ * @n: This is the entry
+ *
+ * Return: Always 0.
+ */
 int print_sign(int n)
 {
-	if (n % 10 > 0)
+        if (n > 0)
 	{
-		_putchar ('+');
+		_putchar('+');
 		return (1);
 	}
-	else if (n % 10 == 0)
+        else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+        else
 	{
 		_putchar('0');
-		return(0);
-	}
-	else
-	{
-		_putchar(45);
-		return(-1);
+		return (0);
 	}
 }
