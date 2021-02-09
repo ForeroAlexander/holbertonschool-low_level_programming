ca50c0c59e994b9636d8882426d2b115c3f9809b
ForeroAlexander <foreroalex26@gmail.com>
2021-02-09 20:25:08 +0000

    Go

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,22 +8,18 @@
  * Return: largest number
  */
 
-int largest_number(int b, int a, int c)
+int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && b >= c)
-	{
+	if (a > b && a > c)
 		largest = a;
-	}
-	else if (b >= a && a >= c)
-	{
+	else if (b > a && b > c)
 		largest = b;
-	}
+	else if (a == b)
+		largest = a;
 	else
-	{
 		largest = c;
-	}
 
 	return (largest);
 }
--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -13,9 +13,9 @@ int main(void)
 	int day;
 	int year;
 
-	month = 02;
-	day = 29;
-	year = 2000;
+	month = 4;
+	day = 01;
+	year = 1997;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 
