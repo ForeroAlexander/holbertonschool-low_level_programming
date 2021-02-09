9d90295e26c94561cc6bce4b264c27ff85b01935
ForeroAlexander <foreroalex26@gmail.com>
2021-02-09 23:21:36 +0000

    C

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,17 @@
  */
 int print_sign(int n)
 {
-        if (n > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-        else if (n < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
 	}
-        else
+	else
 	{
 		_putchar('0');
 		return (0);
