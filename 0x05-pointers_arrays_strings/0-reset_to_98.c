f4f4cd2893c740df9e25ee403124a71f84842a03
ForeroAlexander <foreroalex26@gmail.com>
2021-02-17 03:13:55 +0000

    GO

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 /**
  *
- *reset_to_98 akes a pointer to an int as parameter and updates the value it points to to 98.
+ *reset_to_98 - akes a pointer to an int as parameter and updates the value 
  *
- *@n int: pointer
+ *@n: pointer
  *
  */
 void reset_to_98(int *n)
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,18 +1,17 @@
 #include "holberton.h"
-#include <stdio.h>
+
 /**
- *swap_int - function that swaps the values of two integer
- *@a: pointer
- *@b: pointer
+ * swap_int - This function changed the entry
+ * @a: this is the first entry
+ * @b: this is the second entry
  *
-*Return : Always 0 (Sucess)
  *
  */
 void swap_int(int *a, int *b)
 {
-	int temp;
+	int changed;
 
-	temp = *b;
-	*b = *a;
-	*a = temp;
+	changed = *a;
+	*a = *b;
+	*b = changed;
 }
