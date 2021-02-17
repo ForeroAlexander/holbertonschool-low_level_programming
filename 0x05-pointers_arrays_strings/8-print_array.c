a3ed37bac3e9966cc5ce58818ea4993766dd562c
ForeroAlexander <foreroalex26@gmail.com>
2021-02-17 04:01:15 +0000

    GO

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+ #include <stdio.h>
+
+/**
+ * print_array - prototipo
+ *
+ * @a : variable puntero
+ *
+ * @n : variable
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int num;
+	for (num = 0 ; num < n ; num++)
+	{
+		printf("%d", a[num]);
+		if (num != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
