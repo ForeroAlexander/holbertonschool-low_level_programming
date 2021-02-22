46521174642871ec4295ed55997dac1932ff8399
ForeroAlexander <foreroalex26@gmail.com>
2021-02-22 23:49:24 +0000

    GO

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
- *_memset - function that fills memory with a constant byte
- *@s: variable
- *@b: variable
+ *_memcpy - function that fills memory with a constant byte
+ *@dest: variable
+ *@src: variable
  *@n: variable
- * Return: s
+ * Return: dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
@@ -14,5 +14,5 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	for (i = 0; i < n; i++)
 		*(dest + 1) = *(src + 1);
 
-	return (s);
+	return (dest);
 }
