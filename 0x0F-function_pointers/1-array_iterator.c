3173ece31a899015a5a82ea19d5d6768c6bd2002
ForeroAlexander <foreroalex26@gmail.com>
2021-03-10 18:15:01 +0000

    #2

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - print array in Hexadecimal and Integer
+ * @array: collection of elements of the type same
+ * @size: size of the array
+ * @action: name function pointer
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	if (!array || !action)
+		return;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
