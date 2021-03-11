f62cb02369a5f617a638f5c22a7ee2b4d837e8e2
ForeroAlexander <foreroalex26@gmail.com>
2021-03-11 17:36:01 +0000

    header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,15 +6,4 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 
-typedef struct printargument
-{
-	char *typeargument;
-	void (*func)();
-} printargument;
-
-void print_c(va_list valist);
-void print_i(va_list valist);
-void print_f(va_list valist);
-void print_s(va_list valist);
-
 #endif
