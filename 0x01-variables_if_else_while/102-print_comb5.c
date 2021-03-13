2f3a35cb94703fa7b5da6d031d30776ee227624d
ForeroAlexander <foreroalex26@gmail.com>
2021-03-13 04:44:05 +0000

    Advance #3

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main (void)
+{
+        int n1 = 48;
+        int n2 = 0;
+	int n3;
+
+        while (n2 <= 99)
+        {
+                n3 = n2 + 1;
+                while (n3 <= 99)
+                {
+                        putchar((n2 / 10) + n1);
+			putchar((n2 % 10) + n1);
+                        putchar(32);
+			putchar((n3 / 10) + n1);
+			putchar((n3 / 10) + n1);
+                        if (n1 != 98 || n2 != 99)
+                        {
+                                putchar(44);
+                                putchar(32);
+                        }
+                        n3 += 1;
+                }
+		n2 += 1;
+	}
+		putchar('\n');
+		return (0);
+}
