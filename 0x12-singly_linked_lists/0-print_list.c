06179159338417f16610067cf089327c52b1adda
ForeroAlexander <foreroalex26@gmail.com>
2021-03-19 21:49:06 +0000

    linked list #0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,9 +18,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
+			printf("[%u] %s\n", h->len, h->str);
 		n++;
 		h = h->next;
 	}
