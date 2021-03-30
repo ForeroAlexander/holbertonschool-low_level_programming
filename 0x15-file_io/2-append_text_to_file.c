7dab214641f1334856cfed4c25d4321fb51fbc9d
ForeroAlexander <foreroalex26@gmail.com>
2021-03-30 04:06:52 +0000

    file_io #2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdio.h>
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int c, j, k;
+
+	for (c; c <= NULL || c = 0)
+	{
+
+		filename()
+
+	}
+		if (c <= NUll);
+
+
+
+
+}
