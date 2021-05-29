84d7e278bd6bf36f22753754d918620dced52d0d
ForeroAlexander <foreroalex26@gmail.com>
2021-05-28 21:57:24 -0500

    creates a hash table fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,17 +14,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 
         /* new table */
         new_table = malloc(sizeof(hash_table_t));
-
         if (new_table == NULL)
-                return NULL;
+                return (NULL);
+
+        new_table->array = malloc(sizeof(hash_node_t *) * size);
+        if (new_table == NULL)
+                return (NULL);
 
         new_table->size = size;
-
-        /* void *calloc(size_t, size_t); */
-        new_table->array = calloc(size, sizeof(hash_node_t));
-
-        if (new_table->array == NULL)
-                return (NULL);
 
         return (new_table);
 }
