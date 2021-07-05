ca9e31d6b8f473873816159c93537e19f0a5831e
ForeroAlexander <foreroalex26@gmail.com>
2021-07-04 21:35:44 -0500

    makefile python

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""perimeter of a island"""
+
+
+def island_perimeter(grid):
+    """
+    function def island_perimeter(grid):
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                k, m = i, i
+                h, l = j, j
+                if i == 0:
+                    k = 1
+                    perimeter += 1
+                if j == 0:
+                    h = 1
+                    perimeter += 1
+                if j == len(grid[i]) - 1:
+                    l = j - 1
+                    perimeter += 1
+                if i == len(grid) - 1:
+                    m = i - 1
+                    perimeter += 1
+                if grid[k - 1][j] == 0:
+                    perimeter += 1
+                if grid[i][h - 1] == 0:
+                    perimeter += 1
+                if grid[i][l + 1] == 0:
+                    perimeter += 1
+                if grid[m + 1][j] == 0:
+                    perimeter += 1
+    return perimeter
